dccfe70f5c9eda0361bf117477fb06cd6b766e3a
Wang Yao <wangyao_19941@hotmail.com>
2021-05-13 10:05:44 +0800

    Create create_captcha.py

---
+++ DataPre/create_captcha.py
@@ -0,0 +1,45 @@
+from captcha.image import ImageCaptcha  # 如果没有模块captcha,使用pip install captcha下载
+from PIL import Image
+import random
+import numpy as np
+
+
+# 验证码中的字符
+number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+            'V', 'W', 'X', 'Y', 'Z']
+
+all = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+       'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+       'v', 'w', 'x', 'y', 'z',
+       'AA', 'BB', 'CC', 'DD', 'EE', 'FF', 'GG', 'HH', 'II', 'JJ', 'KK', 'LL', 'MM', 'NN', 'OO', 'PP', 'QQ', 'RR',
+       'SS', 'TT', 'UU', 'VV', 'WW', 'XX', 'YY', 'ZZ']
+
+# 验证码长度4个字符
+def random_captcha_text(char_set=number + alphabet + ALPHABET, captcha_size=4):
+    captcha_text = []
+    for i in range(captcha_size):
+        c = random.choice(char_set)
+        captcha_text.append(c)
+    return captcha_text
+
+
+# 生成字符对应的验证码
+def gen_captcha_text_and_image():
+    image = ImageCaptcha()
+    captcha_text = random_captcha_text()
+    captcha_text = ''.join(captcha_text)
+    captcha = image.generate(captcha_text)
+    dir = r'./data/raw_train/captcha'  # 指定captcha验证码图片的存放路径
+    image.write(captcha_text, dir + captcha_text + '.png')  # 生成验证码时取消注释，写到文件
+
+    captcha_image = Image.open(captcha)
+    captcha_image = np.array(captcha_image)
+    return captcha_text, captcha_image
+
+
+if __name__ == '__main__':
+    for i in range(20000):
+        text, image = gen_captcha_text_and_image()

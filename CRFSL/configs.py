d9b30d363a6ebc1fdf9a02c0dc3f274606388ae5
Wang Yao <wangyao_19941@hotmail.com>
2021-05-13 10:04:53 +0800

    Create configs.py

---
+++ CRFSL/configs.py
@@ -0,0 +1,14 @@
+save_dir                    = '/root/wangyao/zyf/EXP/CRFSL/'
+data_dir = {}
+
+data_dir['wiki']          = './filelists/wiki/'
+data_dir['cnn']          = './filelists/cnn/'
+data_dir['bjyh']          = './filelists/bjyh/'
+data_dir['msyh']          = './filelists/msyh/'
+data_dir['gfyh']          = './filelists/gfyh/'
+
+# data_dir['gen5_wiki']          = './filelists/gen5_wiki/'
+# data_dir['gen5_cnn']          = './filelists/gen5_cnn/'
+# data_dir['gen5_bjyh']          = './filelists/gen5_bjyh/'
+# data_dir['gen5_msyh']          = './filelists/gen5_msyh/'
+# data_dir['gen5_gfyh']          = './filelists/gen5_gfyh/'
